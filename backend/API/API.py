d5b4ae7f16f02ee294cd409233dd8e9451acfce3
Imperator | I always comeback <abdlazimovtima@gmail.com>
2026-06-09 20:37:40 +0500

    бета api
    добавил основу и базовй минимум для мессанджера blist

--- backend/API/API.py
+++ backend/API/API.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 import asyncio
 app = FastAPI()
-@app.post("/Register/")
-async def pri(mass = {}):
-    return mass
-@app.get("/Register/")
-async def exi():
+class reg_person(BaseModel):
+    name: str
+    email: str
+    password: str
+    replay_password: str
+class login_person(BaseModel):
+    name: str
+    password: str
+@app.post("/registration")
+def prin(mass:reg_person):
+    return {"Имя пользователя":mass.name,"Емейл пользователя":mass.email}
+@app.get("/messange")
+def LOOK_messange():
+    return 
+@app.post("/login")
+def login():
     return
-print(asyncio.run(pri()))
+@app.post("/messages")
+def messages():
+    return
